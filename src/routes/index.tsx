ceae8eb7030a6fce21549cb590d643a0e228fb9d
Vicente Yah <tu-email@ejemplo.com>
2026-04-25 05:00:27 -0600

    rote arrow function

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,7 +2,9 @@ import { createFileRoute } from '@tanstack/react-router'
 import { HomePage } from '../pages/HomePage'
 
 export const Route = createFileRoute('/')({
-    component: HomePage,
+    component: () => <HomePage/> ,
 })
+
+console.log('INDEX ROUTE LOADED')
 
 
