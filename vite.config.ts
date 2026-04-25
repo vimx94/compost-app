945537dcdf6315bc605d5e5ccfa84b9be7d8b8c7
Vicente Yah <tu-email@ejemplo.com>
2026-04-25 04:15:30 -0600

    vite config setup finally

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,15 @@ import react from '@vitejs/plugin-react'
 
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
-// https://vite.dev/config/
-export default defineConfig({
-  base: process.env.NODE_ENV === 'production'
-    ? '/compost-app/' 
-    : '/',
-  plugins: [
-              tanstackRouter({
-                target: 'react',
-                autoCodeSplitting: true,
-              }),
-              react()
-            ],
+export default defineConfig(({ mode }) => {
+  return {
+    base: mode === 'production' ? '/compost-app/' : '/',
+    plugins: [
+      tanstackRouter({
+        target: 'react',
+        autoCodeSplitting: true,
+      }),
+      react(),
+    ],
+  }
 })
